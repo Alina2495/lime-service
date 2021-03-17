86f2cbdbc0cbd4a6704007085e6a50856f0edf0d
Alina2495 <48183610+Alina2495@users.noreply.github.com>
2021-03-17 17:40:02 +0300

    17.03.2021

--- js/script.js
+++
@@ -1,5 +0,0 @@
-on("click", ".btn-table-collapse", (function(e) {
-    e.preventDefault(), i()(".table").toggleClass("short");
-    var t = i()(".btn-table-collapse");
-    t.text("Показать прайс полностью" == t.text() ? "Скрыть полный прайс" : "Показать прайс полностью"), Object(a.j)(i()(".mini-price-header").offset().top)
-}))
